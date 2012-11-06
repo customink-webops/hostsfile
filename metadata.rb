2c10f1dbb8b8734f69517e6f9d768bb98a44c455
Seth Vargo <sethvargo@gmail.com>
2012-11-05 21:47:28 -0500

    version bump for new release

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'webops@customink.com'
 license 'All rights reserved'
 description 'Provides an LWRP for managing the /etc/hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.2.0'
