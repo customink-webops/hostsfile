8715014a109c02fbd687c4f061059e7a43bcd922
Seth Vargo <sethvargo@gmail.com>
2014-02-01 17:41:37 -0500

    Version bump to 2.4.3

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'sethvargo@gmail.com'
 license          'Apache 2.0'
 description      'Provides an LWRP for managing the /etc/hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.2'
+version          '2.4.3'
