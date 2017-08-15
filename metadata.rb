b40e98c504aa3f63d1a5a994a040e88d4075857d
Tim Smith <tsmith@chef.io>
2017-08-15 14:58:44 -0700

    Resolve foodcritic warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 name             'hostsfile'
 maintainer       'Seth Vargo'
 maintainer_email 'sethvargo@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Provides an LWRP for managing the /etc/hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.4.6'
 issues_url       'https://github.com/customink-webops/hostsfile/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/customink-webops/hostsfile/nrpe' if respond_to?(:source_url)
 chef_version     '>= 11.0' if respond_to?(:chef_version)
+supports         'all'
